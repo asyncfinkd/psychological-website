706b47fadab38541af8864093cfc38fd29ab0f4f
asyncfinkd <shamgoingzuck@gmail.com>
2021-07-08 00:01:53 +0400

    Footer Fixtures

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,44 +1,30 @@
 import React from "react";
+import { FooterFixtures } from "../../fixtures/footer/FooterFixtures";
 
 export default function Footer() {
   return (
     <>
       <footer>
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-12 col-md-12 col-sm-12">
-              <ul class="social">
-                <li>
-                  <a href="#">
-                    <i class="fa fa-facebook"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <i class="fa fa-twitter"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <i class="fa fa-linkedin"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <i class="fa fa-rss"></i>
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <i class="fa fa-dribbble"></i>
-                  </a>
-                </li>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12 col-md-12 col-sm-12">
+              <ul className="social">
+                {FooterFixtures.map((item, i) => {
+                  const { className, link } = item;
+                  return (
+                    <li key={i}>
+                      <a href={link}>
+                        <i className={className}></i>
+                      </a>
+                    </li>
+                  );
+                })}
               </ul>
             </div>
           </div>
-          <div class="row">
-            <div class="col-lg-12">
-              <p class="copyright helvetica:medium">
+          <div className="row">
+            <div className="col-lg-12">
+              <p className="copyright helvetica:medium">
                 ყველა უფლება დაცულია &copy; 2021 - ბათუმის შოთა რუსთაველის
                 უნივერსიტეტი
               </p>
---
+++ src/fixtures/footer/FooterFixtures.js
@@ -0,0 +1,22 @@
+export const FooterFixtures = [
+  {
+    className: "fa fa-facebook",
+    link: "#",
+  },
+  {
+    className: "fa fa-twitter",
+    link: "#",
+  },
+  {
+    className: "fa fa-linkedin",
+    link: "#",
+  },
+  {
+    className: "fa fa-rss",
+    link: "#",
+  },
+  {
+    className: "fa fa-dribbble",
+    link: "#",
+  },
+];
