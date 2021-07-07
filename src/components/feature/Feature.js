689440cf20d3fda4cb55510f788a4a552b1435eb
asyncfinkd <shamgoingzuck@gmail.com>
2021-07-08 00:45:53 +0400

    Feature Optimization

--- src/components/feature/Feature.js
+++ src/components/feature/Feature.js
@@ -4,26 +4,26 @@ import { FeatureFixtures } from "../../fixtures/feature/FeatureFixtures";
 export default function Feature() {
   return (
     <>
-      <section class="section home-feature">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-12">
-              <div class="row">
+      <section className="section home-feature">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="row">
                 {FeatureFixtures.map((item, i) => {
                   const { image, title, text } = item;
                   return (
                     <div
-                      class="col-lg-4 col-md-6 col-sm-6 col-12"
+                      className="col-lg-4 col-md-6 col-sm-6 col-12"
                       data-scroll-reveal="enter bottom move 50px over 0.6s after 0.2s"
                       key={i}
                     >
-                      <div class="features-small-item">
-                        <div class="icon">
+                      <div className="features-small-item">
+                        <div className="icon">
                           <i>
                             <img src={image} alt="" />
                           </i>
                         </div>
-                        <h5 class="features-title">{title}</h5>
+                        <h5 className="features-title">{title}</h5>
                         <p>{text}</p>
                       </div>
                     </div>
