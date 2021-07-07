85d399f17838df3079acff09236c7d3f6f93f654
asyncfinkd <shamgoingzuck@gmail.com>
2021-07-08 00:50:08 +0400

    Feature Fixtures Upgrade

--- src/fixtures/feature/FeatureFixtures.js
+++ src/fixtures/feature/FeatureFixtures.js
@@ -1,17 +1,19 @@
 export const FeatureFixtures = [
   {
     image: "assets/images/featured-item-01.png",
-    title: "Modern Strategy",
-    text: "Customize anything in this template to fit your website needs",
+    title: "თანამედროვე სტრატეგია",
+    text:
+      "მონიშნეთ ყველაფერი ამ შაბლონში თქვენი ვებსაიტის საჭიროებების შესაბამისად",
   },
   {
     image: "assets/images/featured-item-01.png",
-    title: "Best Relationship",
-    text: "Contact us immediately if you have a question in mind",
+    title: "საუკეთესო ურთიერთობა",
+    text: "დაუყოვნებლივ დაგვიკავშირდით, თუ გაქვთ კითხვა",
   },
   {
     image: "assets/images/featured-item-01.png",
-    title: "Ultimate Marketing",
-    text: "You just need to tell your friends about our free templates",
+    title: "საბოლოო მარკეტინგი",
+    text:
+      "თქვენ უბრალოდ უნდა უთხრათ თქვენს მეგობრებს ჩვენი უფასო შაბლონების შესახებ ",
   },
 ];
