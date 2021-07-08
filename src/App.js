6cd79a3874686d874854aeac5da309344874873d
asyncfinkd <shamgoingzuck@gmail.com>
2021-07-08 11:57:22 +0400

    Delete

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ function App() {
         <div class="header-text">
           <div class="container">
             <div class="row">
-              <div className="bg__blur"></div>
               <div class="offset-xl-3 col-xl-6 offset-lg-2 col-lg-8 col-md-12 col-sm-12">
                 <h1
                   className="helvetica:medium"
