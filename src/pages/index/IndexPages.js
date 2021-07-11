554ff2a33245d7f8115d627966cd8c718df2dfbf
asyncfinkd <shamgoingzuck@gmail.com>
2021-07-11 21:03:47 +0400

    Fix Import Problems

--- src/pages/index/IndexPages.js
+++ src/pages/index/IndexPages.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Feature from "./components/feature/Feature";
-import Footer from "./components/footer/Footer";
-import Navbar from "./components/navbar/Navbar";
+import Feature from "../../components/feature/Feature";
+import Footer from "../../components/footer/Footer";
+import Navbar from "../../components/navbar/Navbar";
 
 export default function IndexPages() {
   return (
