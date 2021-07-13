ced5bd2d1bc8903a624f215d918516a9bd0386fe
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-07-13 19:35:26 +0400

    Admin Dashboard

---
+++ src/components/admin/index/AdminIndex.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function AdminIndex() {
+    return(
+        <>
+            <p>Hello Admin Dashboard</p>
+        </>
+    )
+}
--- src/pages/Admin/AdminPages.js
+++ src/pages/Admin/AdminPages.js
@@ -1,60 +1,19 @@
-import React, { useRef, useState } from "react";
-import "./AdminPages.css";
-import env from "../../application/environment/env.json";
-import axios from "axios";
+import React from 'react';
+import AdminSignin from '../../components/admin/signin/AdminSignin';
+import AdminIndex from "../../components/admin/index/AdminIndex";
 
 export default function AdminPages() {
-    const [email, setEmail] = useState("");
-    const emailRef = useRef();
-    const [emailError, setEmailError] = useState(false);
-    const [password, setPassword] = useState("");
-    const [passwordError, setPasswordError] = useState(false);
-    const passwordRef = useRef();
-    const [showPassword, setShowPassword] = useState(false);
-    const identificationButton = () => {
-        if(!email) {
-            setEmailError(true);
-            emailRef.current.focus();
-            setPasswordError(false);
-        } else if(!password) {
-            setPasswordError(true);
-            passwordRef.current.focus();
-            setEmailError(false);
-        } else {
-            setPasswordError(false);
-            setEmailError(false);
-            axios.post(`${env.host}/auth/readSignin`, {
-                email,
-                password
-            }).then((res) => {
-                if(res.data.success) {
-                    localStorage.setItem("user", JSON.stringify(res.data.user));
-                    localStorage.setItem("logged", true);
-                }
-            })
-        }
-    }
     return(
         <>
-        <div className="form__admin__containerBody">
-            <form onSubmit={e => e.preventDefault()} className="form__admin__container helvetica:regular">
-                <h2 className="form__admin__title">შესვლა ადმინისტრატორით</h2>
-                <div className="position:relative">
-                    <input type="text" ref={emailRef} autoFocus className={emailError ? "form__admin__input form__admin__input__error" : "form__admin__input"} placeholder="ელექტრონული ფოსტა" value={email} onChange={e => setEmail(e.target.value)} /><br/>
-                    {emailError && <small className="form__admin__errorLabel">შეიყვანეთ ელექტრონული ფოსტა</small>}
-                </div>
-                <div className="position:relative">
-                    <input type={showPassword ? "text" : "password"} ref={passwordRef} className={passwordError ? "form__admin__input form__admin__input__error" : "form__admin__input"} placeholder="პაროლი" value={password} onChange={e => setPassword(e.target.value)} /><br/>
-                    {passwordError && <small className="form__admin__errorLabel">შეიყვანეთ პაროლი</small>}
-                    <div className={showPassword ? "form__admin__passwordEye form__admin__passwordShowEye" : "form__admin__passwordEye form__admin__passwordHideEye"} onClick={() => {
-                        setShowPassword(!showPassword)
-                    }}></div>
-                </div>
-                <div>
-                    <button className="form__admin__button" type="submit" onClick={() => identificationButton()}>შესვლა</button>
-                </div>
-            </form>
-            </div>
+            {localStorage.getItem("logged") === "true" ? (
+                <>
+                    <AdminIndex />
+                </>
+            ) : (
+                <>
+                    <AdminSignin />
+                </>
+            )}
         </>
     )
 }
