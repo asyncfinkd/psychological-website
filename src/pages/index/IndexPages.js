6c7e05fedf804db175e18a89048f67a58306688c
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-07-14 17:09:24 +0400

    Fix Some Problems

--- src/pages/index/IndexPages.js
+++ src/pages/index/IndexPages.js
@@ -29,8 +29,8 @@ export default function IndexPages() {
               setDdVar(true);
             
           }else {
-              WelcomeArea.style.backgroundRepeat = "no-repeat";
-              WelcomeArea.style.background = "url('assets/images/1.jpg')";
+            WelcomeArea.style.background = "url('assets/images/1.jpg')";
+            WelcomeArea.style.backgroundRepeat = "no-repeat";
               WelcomeArea.style.backgroundPosition = "center center";
               WelcomeArea.style.backgroundSize = "cover";
               setDd(1);
@@ -299,121 +299,6 @@ export default function IndexPages() {
                     <h3 class="user-name">David Martin</h3>
                     <span>Website Manager</span>
                   </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-
-      <section class="section colored" id="pricing-plans">
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-12">
-              <div class="center-heading">
-                <h2 class="section-title">Pricing Plans</h2>
-              </div>
-            </div>
-            <div class="offset-lg-3 col-lg-6">
-              <div class="center-text">
-                <p>
-                  Donec vulputate urna sed rutrum venenatis. Cras consequat
-                  magna quis arcu elementum, quis congue risus volutpat.
-                </p>
-              </div>
-            </div>
-          </div>
-
-          <div class="row">
-            <div
-              class="col-lg-4 col-md-6 col-sm-12"
-              data-scroll-reveal="enter bottom move 50px over 0.6s after 0.2s"
-            >
-              <div class="pricing-item">
-                <div class="pricing-header">
-                  <h3 class="pricing-title">Starter</h3>
-                </div>
-                <div class="pricing-body">
-                  <div class="price-wrapper">
-                    <span class="currency">$</span>
-                    <span class="price">14.50</span>
-                    <span class="period">monthly</span>
-                  </div>
-                  <ul class="list">
-                    <li class="active">60 GB space</li>
-                    <li class="active">600 GB transfer</li>
-                    <li class="active">Pro Design Panel</li>
-                    <li>15-minute support</li>
-                    <li>Unlimited Emails</li>
-                    <li>24/7 Security</li>
-                  </ul>
-                </div>
-                <div class="pricing-footer">
-                  <a href="#test" class="main-button">
-                    Purchase Now
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            <div
-              class="col-lg-4 col-md-6 col-sm-12"
-              data-scroll-reveal="enter bottom move 50px over 0.6s after 0.4s"
-            >
-              <div class="pricing-item active">
-                <div class="pricing-header">
-                  <h3 class="pricing-title">Premium</h3>
-                </div>
-                <div class="pricing-body">
-                  <div class="price-wrapper">
-                    <span class="currency">$</span>
-                    <span class="price">21.50</span>
-                    <span class="period">monthly</span>
-                  </div>
-                  <ul class="list">
-                    <li class="active">120 GB space</li>
-                    <li class="active">1200 GB transfer</li>
-                    <li class="active">Pro Design Panel</li>
-                    <li class="active">15-minute support</li>
-                    <li>Unlimited Emails</li>
-                    <li>24/7 Security</li>
-                  </ul>
-                </div>
-                <div class="pricing-footer">
-                  <a href="#test" class="main-button">
-                    Purchase Now
-                  </a>
-                </div>
-              </div>
-            </div>
-
-            <div
-              class="col-lg-4 col-md-6 col-sm-12"
-              data-scroll-reveal="enter bottom move 50px over 0.6s after 0.6s"
-            >
-              <div class="pricing-item">
-                <div class="pricing-header">
-                  <h3 class="pricing-title">Advanced</h3>
-                </div>
-                <div class="pricing-body">
-                  <div class="price-wrapper">
-                    <span class="currency">$</span>
-                    <span class="price">42.00</span>
-                    <span class="period">monthly</span>
-                  </div>
-                  <ul class="list">
-                    <li class="active">250 GB space</li>
-                    <li class="active">5000 GB transfer</li>
-                    <li class="active">Pro Design Panel</li>
-                    <li class="active">15-minute support</li>
-                    <li class="active">Unlimited Emails</li>
-                    <li class="active">24/7 Security</li>
-                  </ul>
-                </div>
-                <div class="pricing-footer">
-                  <a href="#test" class="main-button">
-                    Purchase Now
-                  </a>
                 </div>
               </div>
             </div>
