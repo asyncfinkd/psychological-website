abf93218f01faef70f660e671e156126b721b31a
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-07-14 20:26:01 +0400

    Partners Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import AdminPages from "./pages/Admin/AdminPages";
 import IndexPages from "./pages/index/IndexPages";
+import PartnersPages from "./pages/partners/PartnersPages";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <BrowserRouter>
         <Route path="/" exact component={IndexPages} />
         <Route path="/admin" exact component={AdminPages} />
+        <Route path="/partners" exact component={PartnersPages} />
       </BrowserRouter>
     </>
   );
---
+++ src/pages/partners/PartnersPages.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Partners from "../../components/partners/Partners";
+
+export default function PartnersPages() {
+    return(
+        <>
+            <Partners />
+        </>
+    )
+}
