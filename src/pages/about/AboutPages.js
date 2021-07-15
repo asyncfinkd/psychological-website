17c668c7f84d387a132c970fe86a66a563998d4c
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-07-15 16:13:18 +0400

    Update AboutPages.js

--- src/pages/about/AboutPages.js
+++ src/pages/about/AboutPages.js
@@ -1,8 +1,17 @@
-import React from "react";
+import loadjs from "loadjs";
+import React, { useEffect } from "react";
 import About from "../../components/about/About";
 
 export default function AboutPages() {
-   
+    useEffect(() => {
+        setTimeout(() => {
+            loadjs(["/assets/js/custom.js"], "foobar");
+            loadjs.ready("foobar", function () {
+              console.log("good");
+            });
+    
+        });
+      });
     return(
         <>
             <About />
