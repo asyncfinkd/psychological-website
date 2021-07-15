1585f8b27a1381deaec67c006066b492cf3ff39f
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-07-15 13:46:49 +0400

    About Page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -29,6 +29,37 @@ export default function About() {
           </a>
         </span>
       </div>
+      <div style={{marginBottom: "3rem"}}></div>
+      <div className="container">
+          <div className="row">
+              <div className="col-lg-12 col-md-12">
+                  <div className="about__section__title">
+                      <div className="about__title__header">
+                          <h5>ფსიქოლოგიური საკონსულტაციო ცენტრები სტუდენტებისთვის</h5>
+                          <h3 className="about__title__container">
+                          ფსიქოლოგიური კონსულტაციის ცენტრების შექმნა სტუდენტებისთვის საქართველოს უმაღლეს საგანმანათლებლო დაწესებულებებში E-PSY
+                          </h3>
+                          <div className="about__title__div">
+                              <span></span>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+          <div className="row">
+              <div className="col-lg-12 col-md-12 col-sm-12">
+                  <p className="about__title__p"></p>
+                  <p className="about__title__p helvetica:medium color:gray">ფსიქოლოგიური კონსულტაციის ცენტრების შექმნა სტუდენტებისთვის საქართველოს უმაღლეს საგანმანათლებლო დაწესებულებებში E-PSY (617980-EPP-1-2020-1-GE-EPPKA2-CBHE-SP)</p>
+                  <p className="about__title__p helvetica:medium color:gray"><br/>
+E-PSY პროექტის (განხორციელების პერიოდი – 2021-2023 წწ.) ზოგადი მიზანია სტუდენტებისთვის სოციალური და ფსიქოლოგიური დახმარების გაწევა საქართველოს უმაღლეს სასწავლებლებში ფსიქოლოგიური კონსულტაციების ცენტრების შექმნის გზით. ზოგადი მიზნიდან გამომდინარეობს ხუთი კონკრეტული მიზანი:<br/>
+&nbsp; &nbsp; 1. ფსიქოლოგიური ცენტრების შექმნა სტუდენტებისთვის პარტნიორ უმაღლეს სასწავლებლებში;<br/>
+&nbsp; &nbsp; 2. პერსონალის მომზადება და გადამზადება;<br/>
+&nbsp; &nbsp; 3. რეკომენდაციებისა &nbsp;და საკანონმდებლო დოკუმენტაციების შემუშავება;<br/>
+&nbsp; &nbsp; 4. ფსიქიკური ჯანმრთელობის შესახებ სტუდენტებისა და, საერთოდ, ფართო აუდიტორიის ცნობიერების ამაღლება;<br/>
+&nbsp; &nbsp; 5. საქართველოს უნივერსიტეტების ფსიქოლოგიური კონსულტაციის ცენტრების ასოციაციის შექმნა.</p>
+              </div>
+          </div>
+      </div>
         </>
     )
 }
