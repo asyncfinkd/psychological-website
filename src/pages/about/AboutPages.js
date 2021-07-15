c20b2d61b2f6a1bbdd27f62337d22cb8019442ec
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-07-15 17:44:35 +0400

    Delete Some Code

--- src/pages/about/AboutPages.js
+++ src/pages/about/AboutPages.js
@@ -1,17 +1,7 @@
-import loadjs from "loadjs";
-import React, { useEffect } from "react";
+import React from "react";
 import About from "../../components/about/About";
 
 export default function AboutPages() {
-    useEffect(() => {
-        setTimeout(() => {
-            loadjs(["/assets/js/custom.js"], "foobar");
-            loadjs.ready("foobar", function () {
-              console.log("good");
-            });
-    
-        });
-      });
     return(
         <>
             <About />
--- src/pages/partners/PartnersPages.js
+++ src/pages/partners/PartnersPages.js
@@ -1,17 +1,7 @@
-import loadjs from "loadjs";
-import React, { useEffect } from "react";
+import React from "react";
 import Partners from "../../components/partners/Partners";
 
 export default function PartnersPages() {
-    useEffect(() => {
-        setTimeout(() => {
-            loadjs(["/assets/js/custom.js"], "foobar");
-            loadjs.ready("foobar", function () {
-              console.log("good");
-            });
-    
-        });
-      });
     return(
         <>
             <Partners />
