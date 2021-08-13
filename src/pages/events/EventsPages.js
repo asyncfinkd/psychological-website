a2718a76b4284a9c7da552db5ee9edac9474ac6b
asyncfinkd <shamgoingzuck@gmail.com>
2021-08-13 20:15:27 +0400

    Update EventsPages.js

--- src/pages/events/EventsPages.js
+++ src/pages/events/EventsPages.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../../components/navbar/Navbar";
 import { Helmet } from "react-helmet";
+import { EventsContext } from "../../context/events/EventsContext";
 
 export default function EventsPages() {
+  const { events, setEvents } = useContext(EventsContext);
   return (
     <>
       <Helmet>
         <title>
-        ღონისძიებები - ფსიქოლოგიური საკონსულტაციო ცენტრების სტუდენტებისთვის
+          ღონისძიებები - ფსიქოლოგიური საკონსულტაციო ცენტრების სტუდენტებისთვის
         </title>
       </Helmet>
       <Navbar />
@@ -35,6 +37,46 @@ export default function EventsPages() {
         </span>
       </div>
       <div style={{ marginBottom: "3rem" }}></div>
+      <div className="container">
+        <div className="row">
+          {events.map((item, i) => {
+            const { title, description, image, route } = item;
+            return (
+              <>
+                <div className="col-lg-4 col-md-6 col-sm-12" key={i}>
+                  <div className="blog-post-thumb">
+                    <div className="img">
+                      <img src={image} alt="" />
+                    </div>
+                    <div className="blog-content">
+                      <h3>
+                        <Link to={`/${route}`}>{title}</Link>
+                      </h3>
+                      <div
+                        className="text"
+                        style={{
+                          maxHeight: "75px",
+                          overflow: "hidden",
+                          textOverflow: "ellipsis",
+                        }}
+                      >
+                        {description}
+                      </div>
+                      <Link
+                        to={`/${route}`}
+                        className="main-button"
+                        style={{ fontFamily: "BPG Mrgvlovani Caps" }}
+                      >
+                        დაწვრილებით
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
