ffcfdba935f3f8761b505d9894b8828f6e17eb80
asyncfinkd <shamgoingzuck@gmail.com>
2021-08-14 00:03:31 +0400

    Open Slider.

--- src/pages/events/detail/EventsDetailPages.js
+++ src/pages/events/detail/EventsDetailPages.js
@@ -1,13 +1,59 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, {
+  useContext,
+  useEffect,
+  useState,
+  useCallback,
+  useRef,
+} from "react";
 import { Link, useLocation } from "react-router-dom";
 import Footer from "../../../components/footer/Footer";
 import Navbar from "../../../components/navbar/Navbar";
 import { EventsContext } from "../../../context/events/EventsContext";
 import { loadjsUtils } from "./utils/loadjs";
+import LightGallery from "lightgallery/react";
+import lgZoom from "lightgallery/plugins/zoom";
+import "lightgallery/css/lightgallery.css";
+import "lightgallery/css/lg-zoom.css";
+import "lightgallery/css/lg-thumbnail.css";
 
 export default function EventsDetailPages({ match }) {
   const { events, setEvents } = useContext(EventsContext);
   const [data, setData] = useState([]);
+  const { image } = data;
+  const lightGallery = useRef(null);
+  const [items, setItems] = useState([
+    {
+      id: "1",
+      size: "1400-933",
+      src: `${data.image}`,
+      thumb: `${data.image}`,
+    },
+  ]);
+
+  const onInit = useCallback((detail) => {
+    if (detail) {
+      lightGallery.current = detail.instance;
+    }
+  }, []);
+
+  const getItems = useCallback(() => {
+    return items.map((item) => {
+      return (
+        <a
+          key={item.id}
+          data-lg-size={item.size}
+          className="gallery-item"
+          data-src={image}
+        >
+          <img className="img-responsive" src={image} />
+        </a>
+      );
+    });
+  });
+
+  useEffect(() => {
+    lightGallery.current.refresh();
+  });
 
   useEffect(() => {
     loadjsUtils();
@@ -75,7 +121,7 @@ export default function EventsDetailPages({ match }) {
                       marginRight: "8px",
                     }}
                   />
-                  <h5 style={{ marginTop: "5px" }}>06-08-2021</h5>
+                  <h5 style={{ marginTop: "5px" }}>13-08-2021</h5>
                 </div>
                 <h3 className="about__title__container">{data.title}</h3>
                 <div className="about__title__div">
@@ -94,7 +140,21 @@ export default function EventsDetailPages({ match }) {
           </div>
         </div>
       </div>
+
       <div style={{ marginBottom: "3rem" }}></div>
+      <div className="container">
+        <div className="light__gallery__ts">
+          <LightGallery
+            plugins={[lgZoom]}
+            elementClassNames="custom-class-name"
+            onInit={onInit}
+          >
+            {getItems()}
+          </LightGallery>
+        </div>
+      </div>
+      <div style={{ marginBottom: "3rem" }}></div>
+
       <Footer />
     </>
   );
