7772888dabc40f5f7e9f98e06aba09b707823fa8
asyncfinkd <shamgoingzuck@gmail.com>
2021-08-14 17:56:59 +0400

    Add MailOptions, Transporter.

--- server/router/reports/ReportsRouter.js
+++ server/router/reports/ReportsRouter.js
@@ -16,6 +16,23 @@ router.route("/reports").post(async (req, res) => {
     },
   });
 
+  let mailOptions = {
+    from: `psychologicalwebsite@gmail.com`,
+    to: `${email}`,
+    subject: `Result`,
+    html: `
+    Hello
+    `,
+  };
+
+  transporter.sendMail(mailOptions, function (err, data) {
+    if (err) {
+      console.log("error occurs: ", err);
+    } else {
+      console.log("email sent");
+    }
+  });
+
   const ReportsSchema = new Reports({
     fullName: fullName,
     email: email,
