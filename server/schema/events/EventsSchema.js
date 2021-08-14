0704463da381fe8e12f58796bcf073749f3ecbcc
asyncfinkd <shamgoingzuck@gmail.com>
2021-08-14 11:39:09 +0400

    Change Schema

--- server/schema/events/EventsSchema.js
+++ server/schema/events/EventsSchema.js
@@ -5,6 +5,18 @@ const EventsSchema = mongoose.Schema({
   title: {
     type: String,
   },
+  description: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  route: {
+    type: String,
+  },
+  date: {
+    type: String,
+  },
 });
 
 const Events = mongoose.model("events", EventsSchema);
