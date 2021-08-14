62c90217e77aa8b91d19cb03fd66a2caa7f22803
asyncfinkd <shamgoingzuck@gmail.com>
2021-08-14 17:55:17 +0400

    CreateTransport in nodeMailer

--- server/router/reports/ReportsRouter.js
+++ server/router/reports/ReportsRouter.js
@@ -1,11 +1,20 @@
 const router = require("express").Router();
 const Reports = require("../../schema/reports/ReportsSchema");
+const nodemailer = require("nodemailer");
 
 router.route("/reports").post(async (req, res) => {
   const fullName = req.body.fullName;
   const email = req.body.email;
   const message = req.body.message;
   const date = req.body.date;
+
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "psychologicalwebsite@gmail.com",
+      pass: "nikanikonika",
+    },
+  });
 
   const ReportsSchema = new Reports({
     fullName: fullName,
