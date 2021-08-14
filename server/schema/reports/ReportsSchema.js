533cc22b84f5a9053b9d0aa0ab58a6b0195216f9
asyncfinkd <shamgoingzuck@gmail.com>
2021-08-14 13:54:45 +0400

    Update Host, Report Schema

--- server/schema/reports/ReportsSchema.js
+++ server/schema/reports/ReportsSchema.js
@@ -4,15 +4,19 @@ mongoose.Promise = global.Promise;
 const ReportsSchema = mongoose.Schema({
   fullName: {
     type: String,
+    required: true,
   },
   email: {
     type: String,
+    required: true,
   },
   message: {
     type: String,
+    required: true,
   },
   date: {
     type: String,
+    required: true,
   },
 });
 
