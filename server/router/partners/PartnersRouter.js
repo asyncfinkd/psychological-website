f0e03d94e14a004a5652514e4e733dfe04aaba5b
asyncfinkd <shamgoingzuck@gmail.com>
2021-08-16 20:26:39 +0400

    More Safe

--- server/router/partners/PartnersRouter.js
+++ server/router/partners/PartnersRouter.js
@@ -3,7 +3,15 @@ const Partners = require("../../schema/partners/PartnersSchema");
 
 router.route("/partners").post(async (req, res) => {
   Partners.find().then((result) => {
-    res.json(result);
+    let data = [];
+    result.map((item) => {
+      data.push({
+        image: item.image,
+        route: item.route,
+        title: item.title,
+      });
+    });
+    res.json(data);
   });
 });
 
