d0d01930bb410f85cefcb8e3b8038fdc94ab5d91
asyncfinkd <shamgoingzuck@gmail.com>
2021-08-17 00:07:20 +0400

    Edit/Delete Button

--- src/pages/Admin/partners/AdminPartnersPages.js
+++ src/pages/Admin/partners/AdminPartnersPages.js
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { loadjsUtils } from "../../events/detail/utils/loadjs";
 import env from "../../../application/environment/env.json";
+import AdminPartnersPagesMap from "./map/AdminPartnersPagesMap";
 
 export default function AdminPartnersPages() {
   const { clicked, partners } = useContext(EventsContext);
@@ -178,83 +179,13 @@ export default function AdminPartnersPages() {
                 {partners.map((item) => {
                   return (
                     <>
-                      <div
-                        className="flex__partner__container"
-                        style={{ marginLeft: "15px" }}
-                      >
-                        <div
-                          className="row no-gutters"
-                          style={{ alignItems: "center" }}
-                        >
-                          <div className="col-md-3 col-lg-3">
-                            <div className="partner__featured__thumbnail">
-                              <img
-                                src={
-                                  item.type === "url"
-                                    ? item.image
-                                    : `${env.host}/public/${item.image}`
-                                }
-                                className="img-fluid partners__image__fluid"
-                                style={{ height: "100px" }}
-                                alt="qwe"
-                              />
-                            </div>
-                          </div>
-                          <div className="col-md-9 col-lg-9">
-                            <div className="partner__featured__content">
-                              <div>
-                                <span className="partner__featured__metaLine">
-                                  <a
-                                    href={item.route}
-                                    target="_blank"
-                                    rel="noreferrer"
-                                  >
-                                    <img
-                                      src="https://epsy.ge/images/icons/web.svg"
-                                      alt=""
-                                    />
-                                    <span>{item.route}</span>
-                                  </a>
-                                </span>
-                              </div>
-                              <div>
-                                <h5 className="partner__featured__title">
-                                  {item.title}
-                                </h5>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div
-                        style={{
-                          marginLeft: "15px",
-                          marginBottom: "30px",
-                          flexWrap: "wrap",
-                          gap: "10px",
-                          display: "flex",
-                          alignItems: "center",
-                        }}
-                      >
-                        <button
-                          className="btn btn-danger"
-                          style={{
-                            fontFamily: "BPG Mrgvlovani Caps",
-                            fontSize: "12px",
-                          }}
-                        >
-                          წაშლა
-                        </button>
-                        <button
-                          className="btn btn-success"
-                          style={{
-                            fontFamily: "BPG Mrgvlovani Caps",
-                            fontSize: "12px",
-                          }}
-                        >
-                          რედაქტირება
-                        </button>
-                      </div>
+                      <AdminPartnersPagesMap
+                        image={item.image}
+                        route={item.route}
+                        title={item.title}
+                        host={env.host}
+                        type={item.type}
+                      />
                     </>
                   );
                 })}
---
+++ src/pages/Admin/partners/map/AdminPartnersPagesMap.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+
+export default function AdminPartnersPagesMap({
+  image,
+  route,
+  title,
+  host,
+  type,
+}) {
+  const [edit, setEdit] = useState(false);
+  return (
+    <>
+      <div className="flex__partner__container" style={{ marginLeft: "15px" }}>
+        <div className="row no-gutters" style={{ alignItems: "center" }}>
+          <div className="col-md-3 col-lg-3">
+            <div className="partner__featured__thumbnail">
+              <img
+                src={type === "url" ? image : `${host}/public/${image}`}
+                className="img-fluid partners__image__fluid"
+                style={{ height: "100px" }}
+                alt="qwe"
+              />
+            </div>
+          </div>
+          <div className="col-md-9 col-lg-9">
+            <div className="partner__featured__content">
+              <div>
+                <span className="partner__featured__metaLine">
+                  <a href={route} target="_blank" rel="noreferrer">
+                    <img src="https://epsy.ge/images/icons/web.svg" alt="" />
+                    <span>{route}</span>
+                  </a>
+                </span>
+              </div>
+              <div>
+                <h5 className="partner__featured__title">{title}</h5>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        style={{
+          marginLeft: "15px",
+          marginBottom: "30px",
+          flexWrap: "wrap",
+          gap: "10px",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <button
+          className="btn btn-danger"
+          style={{
+            fontFamily: "BPG Mrgvlovani Caps",
+            fontSize: "12px",
+          }}
+        >
+          წაშლა
+        </button>
+        <button
+          className="btn btn-success"
+          style={{
+            fontFamily: "BPG Mrgvlovani Caps",
+            fontSize: "12px",
+          }}
+        >
+          რედაქტირება
+        </button>
+      </div>
+    </>
+  );
+}
