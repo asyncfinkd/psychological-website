d6e80585382c68cae6be84d946dc56c976e2158b
asyncfinkd <shamgoingzuck@gmail.com>
2021-08-18 20:25:42 +0400

    Change Image With URL

--- src/pages/Admin/partners/map/AdminPartnersPagesMap.js
+++ src/pages/Admin/partners/map/AdminPartnersPagesMap.js
@@ -13,6 +13,8 @@ export default function AdminPartnersPagesMap({
 }) {
   const [edit, setEdit] = useState(false);
   const [titleValue, setTitleValue] = useState(title);
+  const [imageURLValue, setImageURLValue] = useState(image);
+  const [imageUploadValue, setImageUploadValue] = useState(image);
   const [routeValue, setRouteValue] = useState(route);
   const editRoute = () => {
     window.scrollTo(0, 0);
@@ -23,6 +25,9 @@ export default function AdminPartnersPagesMap({
         title: title,
         changedTitle: titleValue,
         changedRoute: routeValue,
+        type: type,
+        url: imageURLValue,
+        changeUpload: imageUploadValue,
       })
       .then((res) => {
         document.body.classList.remove("append__body");
@@ -51,12 +56,38 @@ export default function AdminPartnersPagesMap({
         <div className="row no-gutters" style={{ alignItems: "center" }}>
           <div className="col-md-3 col-lg-3">
             <div className="partner__featured__thumbnail">
-              <img
-                src={type === "url" ? image : `${host}/public/${image}`}
-                className="img-fluid partners__image__fluid"
-                style={{ height: "100px" }}
-                alt="qwe"
-              />
+              {edit ? (
+                <>
+                  {type === "url" ? (
+                    <>
+                      <textarea
+                        class="form-control"
+                        id="exampleFormControlTextarea1"
+                        rows="1"
+                        style={{
+                          fontFamily: "BPG Mrgvlovani Caps",
+                          fontSize: "13px",
+                        }}
+                        value={imageURLValue}
+                        onChange={(e) => setImageURLValue(e.target.value)}
+                      ></textarea>
+                    </>
+                  ) : (
+                    <>
+                      <p>qwe</p>
+                    </>
+                  )}
+                </>
+              ) : (
+                <>
+                  <img
+                    src={type === "url" ? image : `${host}/public/${image}`}
+                    className="img-fluid partners__image__fluid"
+                    style={{ height: "100px" }}
+                    alt="qwe"
+                  />
+                </>
+              )}
             </div>
           </div>
           <div className="col-md-9 col-lg-9">
