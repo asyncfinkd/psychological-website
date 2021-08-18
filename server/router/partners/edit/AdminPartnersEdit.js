1e20f12664543afac00098d23e3d16d8160b16a7
asyncfinkd <shamgoingzuck@gmail.com>
2021-08-18 20:17:52 +0400

    Change Image backend.

--- server/router/partners/edit/AdminPartnersEdit.js
+++ server/router/partners/edit/AdminPartnersEdit.js
@@ -5,10 +5,21 @@ router.route("/partners/change").post(async (req, res) => {
   const title = req.body.title;
   const changedTitle = req.body.changedTitle;
   const changedRoute = req.body.changedRoute;
+  const type = req.body.type;
+  if (type === "url") {
+    var changedURL = req.body.url;
+  } else {
+    var changedUpload = req.body.changedUpload;
+  }
 
   Partners.findOne({ title: title }).then((result) => {
     result.title = changedTitle;
     result.route = changedRoute;
+    if (type === "url") {
+      result.image = changedURL;
+    } else {
+      result.image = changedUpload;
+    }
     result.save();
     res.json({ success: true });
   });
