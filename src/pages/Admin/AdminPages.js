fbd2dde5d22fc596c02f505f2e567fd72c05f883
asyncfinkd <shamgoingzuck@gmail.com>
2021-08-22 12:54:39 +0400

    Alert in admin Panel

--- src/pages/Admin/AdminPages.js
+++ src/pages/Admin/AdminPages.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import AdminSignin from '../../components/admin/signin/AdminSignin';
+import React, { useState } from "react";
+import AdminSignin from "../../components/admin/signin/AdminSignin";
 import AdminIndex from "../../components/admin/index/AdminIndex";
+import Swal from "sweetalert2";
 
 export default function AdminPages() {
-    return(
+  const renderAdminPanel = () => {
+    const [local, setLocal] = useState(false);
+    if (localStorage.getItem("admin__modal") != "true" || local == false) {
+      Swal.fire(
+        "გაფრთხილება!",
+        "ნებისმიერმა თქვენმა მოქმედებამ შეიძლება შეცვალოს ვებ-საიტის სისტემა",
+        "info"
+      ).then(() => {
+        localStorage.setItem("admin__modal", true);
+        setLocal(true);
+      });
+    } else {
+      return <AdminIndex />;
+    }
+  };
+  return (
+    <>
+      {localStorage.getItem("logged") === "true" ? (
+        <>{renderAdminPanel()}</>
+      ) : (
         <>
-            {localStorage.getItem("logged") === "true" ? (
-                <>
-                    <AdminIndex />
-                </>
-            ) : (
-                <>
-                    <AdminSignin />
-                </>
-            )}
+          <AdminSignin />
         </>
-    )
+      )}
+    </>
+  );
 }
