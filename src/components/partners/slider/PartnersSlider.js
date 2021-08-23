844824ddb042d3949b795fe283b58d13e7046d81
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-08-23 13:23:46 +0400

    Delete Alert

--- src/components/partners/slider/PartnersSlider.js
+++ src/components/partners/slider/PartnersSlider.js
@@ -1,32 +1,36 @@
 import React, { useContext } from "react";
 import "./PartnersSlider.css";
-import {EventsContext} from "../../../context/events/EventsContext";
+import { EventsContext } from "../../../context/events/EventsContext";
 
 export default function PartnersSlider() {
-    const {partners} = useContext(EventsContext);
-    return(
-        <>
-            <div className="container">
-                <h3 className="partnersSlider__title">პარტნიორები</h3>
-                <div className="about__title__div">
-                  <span></span>
+  const { partners } = useContext(EventsContext);
+  return (
+    <>
+      <div className="container">
+        <h3 className="partnersSlider__title">პარტნიორები</h3>
+        <div className="about__title__div">
+          <span></span>
+        </div>
+        <div className="partnersSlider__container" id="scroll__custom">
+          {partners.map((item) => {
+            {
+              console.log(item);
+            }
+            return (
+              <>
+                <div
+                  className="partnersSlider__content"
+                  onClick={() => {
+                    window.open(`${item.route}`, "_blank");
+                  }}
+                >
+                  <img src={item.image} alt="" />
                 </div>
-                <div className="partnersSlider__container" id="scroll__custom">
-                    {partners.map((item) => {
-                        {console.log(item)}
-                        return (
-                            <>
-                                <div className="partnersSlider__content" onClick={() => {
-                                            window.open(`${item.route}`, '_blank');
-
-                                }}>
-                                    <img src={item.image} alt="" />
-                                </div>
-                            </>
-                        )
-                    })}
-                </div>
-            </div>
-        </>
-    )
+              </>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 }
--- src/pages/Admin/AdminPages.js
+++ src/pages/Admin/AdminPages.js
@@ -4,25 +4,12 @@ import AdminIndex from "../../components/admin/index/AdminIndex";
 import Swal from "sweetalert2";
 
 export default function AdminPages() {
-  const renderAdminPanel = () => {
-    const [local, setLocal] = useState(false);
-    if (localStorage.getItem("admin__modal") != "true" || local == false) {
-      Swal.fire(
-        "გაფრთხილება!",
-        "ნებისმიერმა თქვენმა მოქმედებამ შეიძლება შეცვალოს ვებ-საიტის სისტემა",
-        "info"
-      ).then(() => {
-        localStorage.setItem("admin__modal", true);
-        setLocal(true);
-      });
-    } else {
-      return <AdminIndex />;
-    }
-  };
   return (
     <>
       {localStorage.getItem("logged") === "true" ? (
-        <>{renderAdminPanel()}</>
+        <>
+          <AdminIndex />
+        </>
       ) : (
         <>
           <AdminSignin />
