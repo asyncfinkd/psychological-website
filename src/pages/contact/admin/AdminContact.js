850f33d6db3c480f255e794fd89c121281e4cb78
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-08-24 15:33:35 +0400

    Helmet

--- src/pages/contact/admin/AdminContact.js
+++ src/pages/contact/admin/AdminContact.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import Navbar from "../../../components/admin/navbar/AdminNavbar";
 import { loadjsUtils } from "../../events/detail/utils/loadjs";
 import { useLocation, Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 export default function AdminContact() {
   useEffect(() => {
@@ -13,6 +14,9 @@ export default function AdminContact() {
   }, [pathname]);
   return(
     <>
+    <Helmet>
+      <title>კონტაქტი - ფსიქოლოგიური საკონსულტაციო ცენტრების სტუდენტებისთვის</title>
+    </Helmet>
       <Navbar />
     </>
   )
