20cdcd7389e3e438781b5c851be9461019a77e65
Nika Shamiladze <shamgoingzuck@gmail.com>
2021-08-24 15:30:43 +0400

    Default Page Effects

--- src/pages/contact/admin/AdminContact.js
+++ src/pages/contact/admin/AdminContact.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Navbar from "../../../components/admin/navbar/AdminNavbar";
+import { loadjsUtils } from "../../events/detail/utils/loadjs";
+import { useLocation, Link } from "react-router-dom";
 
 export default function AdminContact() {
+  useEffect(() => {
+    loadjsUtils();
+  })
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
   return(
     <>
-      <p>Hello</p>
+      <Navbar />
     </>
   )
 }
