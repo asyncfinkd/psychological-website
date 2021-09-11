271921363f214f62d820e73941edb9b8323a8894
asyncfinkd <shamgoingzuck@gmail.com>
2021-09-11 19:15:01 +0400

    Scroll in Admin About Page

--- src/pages/Admin/about/aboutFeature/AboutFeature.js
+++ src/pages/Admin/about/aboutFeature/AboutFeature.js
@@ -47,7 +47,13 @@ export default function AboutFeature({ title, description, i }) {
   return (
     <>
       <div className="col-lg-4 col-md-6 col-sm-6 col-12" key={i}>
-        <div className="features-small-item">
+        <div
+          className={
+            editStatus
+              ? "features-small-item scroll__for__FeatureSmallItem"
+              : "features-small-item"
+          }
+        >
           <div className="icon">
             <i>
               <img src="../assets/images/featured-item-01.png" alt="" />
